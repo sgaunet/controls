1f6eaaa8e472556cc93615e7d3d7852c184063c0
Sylvain Gaunet <1552102+sgaunet@users.noreply.github.com>
2021-06-27 22:37:29 +0200

    Remove tmp files, check return code

--- tst/tests.sh
+++ tst/tests.sh
@@ -7,11 +7,22 @@ tstDir=$(pwd)
 
 projectWorkdir=$(dirname $tstDir)
 
+
+test -f "$tstDir/result.html" && \rm "$tstDir/result.html"
+test -f "$tstDir/result.md"  && \rm "$tstDir/result.md"
+
 # Generate controls.yaml file with template controls.tmpl
 sed "s#TSTDIR#$tstDir#g" "$tstDir/controls.tmpl" > "$tstDir/controls.yaml"
 
 cd "$projectWorkdir/src"
 go run . -f "$tstDir/controls.yaml" -o "$tstDir/result.md"
+rc=$?
+
+if [ "$rc" != "0" ]
+then
+    echo "Error when executing go run ..."
+    exit 1
+fi
 
 
 # mdtohtml is also a project maintained by the author (forker originaly)
@@ -23,6 +34,6 @@ rc=$?
 if [ "$rc" = "0" ]
 then
     mdtohtml "$tstDir/result.md" "$tstDir/result.html"
-    firefox "$tstDir/result.html"
+    firefox "$tstDir/result.html" &
 fi
 
