581cc0d5e5dc763d274baedeee70afce63cb19b5
Sylvain Gaunet <3924333-sgaunet@users.noreply.gitlab.com>
2021-09-23 13:43:02 +0200

    create the pdf directly

--- tst/tests.sh
+++ tst/tests.sh
@@ -15,7 +15,7 @@ test -f "$tstDir/result.md"  && \rm "$tstDir/result.md"
 sed "s#TSTDIR#$tstDir#g" "$tstDir/controls.tmpl" > "$tstDir/controls.yaml"
 
 cd "$projectWorkdir/src"
-go run . -f "$tstDir/controls.yaml" -o "$tstDir/result.md"
+go run . -f "$tstDir/controls.yaml" -o "$tstDir/result.pdf"
 rc=$?
 
 if [ "$rc" != "0" ]
@@ -23,17 +23,3 @@ then
     echo "Error when executing go run ..."
     exit 1
 fi
-
-
-# mdtohtml is also a project maintained by the author (forker originaly)
-# This command read markdown format and generate html (use wkhtmltopdf after to get a PDF)
-# https://github.com/sgaunet/mdtohtml
-which mdtohtml > /dev/null 2>&1
-rc=$?
-
-if [ "$rc" = "0" ]
-then
-    mdtohtml "$tstDir/result.md" "$tstDir/result.html"
-    firefox "$tstDir/result.html" &
-fi
-
