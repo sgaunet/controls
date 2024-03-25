d92eae2baacfe0434e0dc43acb506cb3314ea382
Sylvain <1552102+sgaunet@users.noreply.github.com>
2024-03-25 09:19:12 +0100

    fix deprecated call

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,7 +2,7 @@ package config
 
 import (
 	"fmt"
-	"io/ioutil"
+	"os"
 
 	"github.com/fatih/color"
 	"gopkg.in/yaml.v2"
@@ -11,7 +11,7 @@ import (
 func ReadyamlConfigFile(filename string) (YamlConfig, error) {
 	var yamlConfig YamlConfig
 
-	yamlFile, err := ioutil.ReadFile(filename)
+	yamlFile, err := os.ReadFile(filename)
 	if err != nil {
 		fmt.Printf("Error reading YAML file: %s\n", err)
 		return yamlConfig, err
